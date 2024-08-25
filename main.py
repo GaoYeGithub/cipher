009ecff12646f81b19efd2552c0ef3f04655ae82
GaoYeGithub <152664000+GaoYeGithub@users.noreply.github.com>
2024-08-25 21:38:26 +0000

    Added history, generate random key, case senstivity, and stlying in bootstrap

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+from flask import Flask, render_template, request
+import random
+
+app = Flask(__name__)
+
 characters = "abcdefghijklmnopqrstuvwxyz"
 characters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 characters += "1234567890"
@@ -6,93 +11,89 @@ characters += "\"\\"
 
 character_count = len(characters)
 
-print("Supported Characters:\n" + characters + "\n")
+def adjust_key_length(key, message):
+    return (key * (len(message) // len(key)) + key[:len(message) % len(key)]) if key else ""
 
 def encrypt_character(plain, key):
-  key_code = characters.index(key)
-  plain_code = characters.index(plain)
-
-  cipher_code = (key_code + plain_code) % character_count
-
-  cipher = characters[cipher_code]
-
-  return cipher
+    key_code = characters.index(key)
+    plain_code = characters.index(plain)
+    cipher_code = (key_code + plain_code) % character_count
+    cipher = characters[cipher_code]
+    return cipher
 
 def encrypt(plain, key):
-  cipher = ""
-
-  for (plain_index, plain_character) in enumerate(plain):
-    key_index = plain_index % len(key)
-    key_character = key[key_index]
-
-    cipher_character = encrypt_character(key_character, plain_character)
-
-    cipher += cipher_character
-
-  return cipher
+    cipher = ""
+    for (plain_index, plain_character) in enumerate(plain):
+        key_index = plain_index % len(key)
+        key_character = key[key_index]
+        cipher_character = encrypt_character(plain_character, key_character)
+        cipher += cipher_character
+    return cipher
 
 def invert_character(character):
-  character_code = characters.index(character)
-
-  inverted_code = (character_count - character_code) % character_count
-  inverted_character = characters[inverted_code]
-
-  return inverted_character
+    character_code = characters.index(character)
+    inverted_code = (character_count - character_code) % character_count
+    inverted_character = characters[inverted_code]
+    return inverted_character
 
 def invert(text):
-  inverted_text = ""
-
-  for character in text:
-    inverted_text += invert_character(character)
-
-  return inverted_text
+    inverted_text = ""
+    for character in text:
+        inverted_text += invert_character(character)
+    return inverted_text
 
 def decrypt_character(cipher, key):
-  key_code = characters.index(key)
-  cipher_code = characters.index(cipher)
-  plain_code = (cipher_code - key_code) % character_count
-  plain = characters[plain_code]
-  return plain
+    key_code = characters.index(key)
+    cipher_code = characters.index(cipher)
+    plain_code = (cipher_code - key_code) % character_count
+    plain = characters[plain_code]
+    return plain
 
 def decrypt(cipher, key):
-  plain = ""
+    plain = ""
+    for (cipher_index, cipher_character) in enumerate(cipher):
+        key_index = cipher_index % len(key)
+        key_character = key[key_index]
+        plain_character = decrypt_character(cipher_character, key_character)
+        plain += plain_character
+    return plain
 
-  for (cipher_index, cipher_character) in enumerate(cipher):
-      key_index = cipher_index % len(key)
-      key_character = key[key_index]
+def random_key(length):
+    return ''.join(random.choice(characters) for _ in range(length))
 
-      plain_character = decrypt_character(cipher_character, key_character)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    result = None
+    history = []
+    key = ''  # Initialize key variable
 
-      plain += plain_character
+    if request.method == 'POST':
+        message = request.form['message']
+        key = request.form['key']
+        operation = request.form['operation']
+        is_case_sensitive = 'case_sensitive' in request.form
+        generate_random_key = request.form.get('generate_random_key') == 'true'
 
-  return plain
+        if not is_case_sensitive:
+            message = message.lower()
+            key = key.lower()
 
+        if generate_random_key:
+            key_length = len(message)
+            key = random_key(key_length)
+        else:
+            key = request.form['key']
 
+        if operation == 'Encrypt':
+            result = '!' + encrypt(message, key)
+        else:
+            if message.startswith('!'):
+                message = message[1:]
+            result = decrypt(message, key)
 
-while True:
-  plaintext = input("Message: ")
-  keytext = input("Password: ")
+        history.append({'operation': operation, 'message': message, 'key': key, 'result': result})
 
-  encrypted = plaintext.startswith("!")
+    return render_template('index.html', result=result, key=key, history=history)
 
-  if encrypted:
-    plaintext = plaintext[1:]
-    keytext = invert(keytext)
-
-  ciphertext = encrypt(plaintext, keytext)
-
-  if not encrypted:
-    ciphertext = "!" + ciphertext
-
-  print("Output: " + ciphertext)
-  print()
-
-  ciphertext = input("Encrypted Message")
-  keytext = input("Password")
-
-  if ciphertext.startswith("!"):
-    ciphertext = ciphertext[1:]
-    keytext = invert(keytext)
-
-  plaintext = decrypt(ciphertext, keytext)
-  print("Decrypted Message: " + plaintext)
+if __name__ == '__main__':
+    app.run(debug=True)
